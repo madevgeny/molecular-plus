04a3a806df3956a50b80773d1a59c2ab3f150987
evgeny <evgeny.podjachev@gmail.com>
2024-02-16 22:44:30 +0300

    More structures ti cpp.

--- c_sources/core.pyx
+++ c_sources/core.pyx
@@ -68,6 +68,78 @@ cdef extern from "structs.h":
         float ebroken
         float friction
 
+    cdef struct ParSys:
+        int id
+        int parnum
+        Particle *particles
+        int selfcollision_active
+        int othercollision_active
+        int collision_group
+        float friction
+        float collision_damp
+        int links_active
+        float link_length
+        int link_max
+        float link_tension
+        float link_tensionrand
+        float link_stiff
+        float link_stiffrand
+        float link_stiffexp
+        float link_damp
+        float link_damprand
+        float link_broken
+        float link_brokenrand
+        float link_estiff
+        float link_estiffrand
+        float link_estiffexp
+        float link_edamp
+        float link_edamprand
+        float link_ebroken
+        float link_ebrokenrand
+        int relink_group
+        float relink_chance
+        float relink_chancerand
+        int relink_max
+        float relink_tension
+        float relink_tensionrand
+        float relink_stiff
+        float relink_stiffexp
+        float relink_stiffrand
+        float relink_damp
+        float relink_damprand
+        float relink_broken
+        float relink_brokenrand
+        float relink_estiff
+        float relink_estiffexp
+        float relink_estiffrand
+        float relink_edamp
+        float relink_edamprand
+        float relink_ebroken
+        float relink_ebrokenrand
+        float link_friction
+        int link_group
+        int other_link_active
+
+
+    cdef struct Particle:
+        int id
+        float loc[3]
+        float vel[3]
+        float size
+        float mass
+        int state
+        float weak
+    
+        ParSys *sys
+        int *collided_with
+        int collided_num
+        Links *links
+        int links_num
+        int links_activnum
+        int *link_with
+        int link_withnum
+        vector[int] neighbours
+
 
     cdef struct KDTree:
         int numnodes
@@ -84,7 +156,7 @@ cdef extern from "structs.h":
         int *thread_parent
         int *thread_depth
 
-    
+
     cdef struct Node:
         int index
         char name
@@ -93,7 +165,7 @@ cdef extern from "structs.h":
         SParticle *particle
         Node *left_child
         Node *right_child
-    
+
 
 cdef float fps = 0
 cdef int substep = 0
@@ -1031,7 +1103,7 @@ cdef void update(data):
 
 
 
-cdef void KDTree_create_nodes(KDTree *kdtree,int parnum):#noexcept nogil:
+cdef void KDTree_create_nodes(KDTree *kdtree,int parnum)noexcept nogil:
     cdef int i = 0
     i = 2
     while i < parnum:
@@ -1431,79 +1503,6 @@ cdef void create_link(int par_id, int max_link, int parothers_id=-1)noexcept nog
     free(link)
     # free(par)
     # free(par2)
-
-
-cdef struct ParSys:
-    int id
-    int parnum
-    Particle *particles
-    int selfcollision_active
-    int othercollision_active
-    int collision_group
-    float friction
-    float collision_damp
-    int links_active
-    float link_length
-    int link_max
-    float link_tension
-    float link_tensionrand
-    float link_stiff
-    float link_stiffrand
-    float link_stiffexp
-    float link_damp
-    float link_damprand
-    float link_broken
-    float link_brokenrand
-    float link_estiff
-    float link_estiffrand
-    float link_estiffexp
-    float link_edamp
-    float link_edamprand
-    float link_ebroken
-    float link_ebrokenrand
-    int relink_group
-    float relink_chance
-    float relink_chancerand
-    int relink_max
-    float relink_tension
-    float relink_tensionrand
-    float relink_stiff
-    float relink_stiffexp
-    float relink_stiffrand
-    float relink_damp
-    float relink_damprand
-    float relink_broken
-    float relink_brokenrand
-    float relink_estiff
-    float relink_estiffexp
-    float relink_estiffrand
-    float relink_edamp
-    float relink_edamprand
-    float relink_ebroken
-    float relink_ebrokenrand
-    float link_friction
-    int link_group
-    int other_link_active
-
-
-cdef struct Particle:
-    int id
-    float loc[3]
-    float vel[3]
-    float size
-    float mass
-    int state
-    float weak
-
-    ParSys *sys
-    int *collided_with
-    int collided_num
-    Links *links
-    int links_num
-    int links_activnum
-    int *link_with
-    int link_withnum
-    vector[int] neighbours
 
 
 cdef struct Pool:
--- c_sources/structs.h
+++ c_sources/structs.h
@@ -1,13 +1,10 @@
 #ifndef MOLECULAR_STRUCTS_H
 #define MOLECULAR_STRUCTS_H
 
+#include <vector>
 #include <math.h>
 
-struct SParticle{
-    int id = -1;
-    float loc[3] = {};
-};
-
+struct Particle;
 
 struct Links{
     float lenght = 0;
@@ -22,6 +19,85 @@ struct Links{
     float edamping = 0;
     float ebroken = 0;
     float friction = 0;
+};
+
+struct ParSys{
+    int id = 0;
+    int parnum = 0;
+    Particle *particles = nullptr;
+    int selfcollision_active = 0;
+    int othercollision_active = 0;
+    int collision_group = 0;
+    float friction = 0;
+    float collision_damp = 0;
+    int links_active = 0;
+    float link_length = 0;
+    int link_max = 0;
+    float link_tension = 0;
+    float link_tensionrand = 0;
+    float link_stiff = 0;
+    float link_stiffrand = 0;
+    float link_stiffexp = 0;
+    float link_damp = 0;
+    float link_damprand = 0;
+    float link_broken = 0;
+    float link_brokenrand = 0;
+    float link_estiff = 0;
+    float link_estiffrand = 0;
+    float link_estiffexp = 0;
+    float link_edamp = 0;
+    float link_edamprand = 0;
+    float link_ebroken = 0;
+    float link_ebrokenrand = 0;
+    int relink_group = 0;
+    float relink_chance = 0;
+    float relink_chancerand = 0;
+    int relink_max = 0;
+    float relink_tension = 0;
+    float relink_tensionrand = 0;
+    float relink_stiff = 0;
+    float relink_stiffexp = 0;
+    float relink_stiffrand = 0;
+    float relink_damp = 0;
+    float relink_damprand = 0;
+    float relink_broken = 0;
+    float relink_brokenrand = 0;
+    float relink_estiff = 0;
+    float relink_estiffexp = 0;
+    float relink_estiffrand = 0;
+    float relink_edamp = 0;
+    float relink_edamprand = 0;
+    float relink_ebroken = 0;
+    float relink_ebrokenrand = 0;
+    float link_friction = 0;
+    int link_group = 0;
+    int other_link_active = 0;
+};
+
+struct Particle{
+    int id = 0;
+    float loc[3] = {};
+    float vel[3] = {};
+    float size = 0;
+    float mass = 0;
+    int state = 0;
+    float weak = 0;
+
+    ParSys *sys = nullptr;
+    int *collided_with = nullptr;
+    int collided_num = 0;
+    Links *links = nullptr;
+    int links_num = 0;
+    int links_activnum = 0;
+    int *link_with = nullptr;
+    int link_withnum = 0;
+    std::vector<int> neighbours;
+
+};
+
+struct SParticle{
+    int id = -1;
+    float loc[3] = {};
 };
 
 
